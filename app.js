a777f904dfba5ad3a6f498513f71c2e522ac723e
John-v77 <ionut_vasile85@hotmail.com>
2021-02-23 17:15:58 -0500

    jason file

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const express = require('express')
 const app = express()
+const movies = require('./movies')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/views/home.html')
 })
+
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/views/contact.html')
+})
+
+app.get('/about', (req, res) => {
+	res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', (req, res) => {
+	res.sendFile(__dirname + '/views/works.html')
+})
+
+
+app.get('/works', (req, res) => {
+	res.sendFile(__dirname + '/views/works.html')
+})
+
+
+app.get('/movies', (req, res) =>{
+	res.json({movies})
+})
+
+
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000')
